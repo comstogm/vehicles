4a146dc0cde3db320c8977c78af1ee04dce7bca1
gabriel <comstogm@mail.uc.edu>
2022-08-27 19:14:00 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("In vehicles Main");
+        hello();
+    }
+    public static void hello() {
+        System.out.println("Hello");
+    }
+}
