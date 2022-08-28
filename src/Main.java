fcbdea2f92e2536806ef13aeef716d07bab1a465
gabriel <comstogm@mail.uc.edu>
2022-08-28 14:16:18 -0400

    Lecture video 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static ArrayList<Vehicle> allVehicles = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("In vehicles Main");
-        hello();
+        System.out.println("Second line");
+
+        Vehicle myVehicle = new Vehicle();
+        myVehicle.setGallonsOfGas(10.0);
+        myVehicle.setMilesPerGallon(20);
+        myVehicle.setOdometer(0);
+        allVehicles.add(myVehicle);
+
+        Vehicle yourVehicle = new Vehicle();
+        yourVehicle.setGallonsOfGas(8.0);
+        yourVehicle.setMilesPerGallon(25);
+        yourVehicle.setOdometer(10000);
+        allVehicles.add(yourVehicle);
+
+        Vehicle thirdVehicle = new Vehicle();
+        thirdVehicle.setGallonsOfGas(12.0);
+        thirdVehicle.setMilesPerGallon(15);
+        thirdVehicle.setOdometer(20000);
+        allVehicles.add(thirdVehicle);
+
+
+        drive(100);
+        drive(60);
     }
-    public static void hello() {
-        System.out.println("Hello");
+
+    private static void drive(int milesDriven) {
+        for (Vehicle vehicle: allVehicles) {
+            vehicle.setGallonsOfGas(vehicle.getGallonsOfGas() - (milesDriven / vehicle.getMilesPerGallon()));
+            vehicle.setOdometer(vehicle.getOdometer() + milesDriven);
+            System.out.println("Odometer: " + vehicle.getOdometer() + " | Gallons of Gas: " + vehicle.getGallonsOfGas());
+        }
+
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,29 @@
+public class Vehicle {
+    private int milesPerGallon;
+    private double gallonsOfGas;
+    private int odometer;
+
+    public int getMilesPerGallon() {
+        return milesPerGallon;
+    }
+
+    public void setMilesPerGallon(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+}
